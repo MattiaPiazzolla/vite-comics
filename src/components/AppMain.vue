8b03e16cdce9d2f5969e8fb8ede2fc79deb18768
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-29 16:51:59 +0200

    [UPD] styled main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,10 +4,14 @@ export default {
 }
 </script>
 <template>
-    <main>
-        <h1>ciao</h1>
+    <main class="p-4 text-light">
+        <div class="container">
+            <h5>-->Content goes here<--</h5>
+        </div>
     </main>
 </template>
 <style lang="scss">
-    
+    main{
+        background-color: #1C1C1C;
+    }
 </style>
