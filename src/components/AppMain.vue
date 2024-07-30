fd044cbcef759732cdb6f929eedbcbaf196ef677
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-30 18:33:33 +0200

    [UPD] inserted the comic cards dynamically in the AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,12 +18,12 @@ export default {
     <main class="p-4 text-light">
         <div class="container">
             
-            <AppCard />
             <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 gx-0">
-                <div class="col p-2" v-for="comic, index in comics" :key="index">
+                <AppCard v-for="comic, index in comics" :key="index" :pathImage="comic.thumb" :title="comic.series" />
+                <!-- <div class="col p-2" v-for="comic, index in comics" :key="index">
                     <img :src="comic.thumb" alt="" class="comicCover">
                     <p class="comicTitle mb-3 text-uppercase">{{ comic.series }}</p>
-                </div>
+                </div> -->
             </div>
         </div>
     </main>
@@ -31,11 +31,6 @@ export default {
 <style lang="scss" scoped>
     main{
         background-color: #1C1C1C;
-    }
-    .comicTitle{
-        font-size: 0.7rem;
-        margin-top: 10px;
-        
     }
     
 </style>
--- src/components/partials/AppCard.vue
+++ src/components/partials/AppCard.vue
@@ -1,15 +1,24 @@
 <script>
 export default {
-    
+    props:{
+        pathImage: String,
+        title: String,
+        price: String,
+        type: String
+    }
 }
 </script>
 
-<template lang="">
-    <div>
-        
+<template>
+    <div class="col p-2">
+        <img :src="pathImage" :alt="title" class="comicCover">
+        <p class="comicTitle mb-3 text-uppercase">{{ title }}</p>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+    .comicTitle{
+        font-size: 0.7rem;
+        margin-top: 10px;   
+    }
 </style>
