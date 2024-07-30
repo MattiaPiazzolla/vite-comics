dd072ddd1299edda383a0eacec9c0f9eb806a874
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-30 19:26:51 +0200

    [UPD] added the jumbotron in the appmain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,15 +15,24 @@ export default {
 }
 </script>
 <template>
-    <main class="p-4 text-light">
-        <div class="container">
+    <main class="text-light">
+        <div class="jumbo">
+            <div class="container position-relative h-100">
+                <div class="labelSection">CURRENT SERIES</div>
+            </div>
+        </div>
+
+        <div class="container p-4">
             
             <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 gx-0">
-                <AppCard v-for="comic, index in comics" :key="index" :pathImage="comic.thumb" :title="comic.series" />
+                <AppCard v-for="comic, index in comics" :key="index" :pathImage="comic.thumb" :title="comic.series" :price="comic.price"/>
                 <!-- <div class="col p-2" v-for="comic, index in comics" :key="index">
                     <img :src="comic.thumb" alt="" class="comicCover">
                     <p class="comicTitle mb-3 text-uppercase">{{ comic.series }}</p>
                 </div> -->
+            </div>
+            <div class="w-100 d-flex justify-content-center mb-0">
+                <button class="btnLoad py-2 px-4 text-light">LOAD MORE</button>
             </div>
         </div>
     </main>
@@ -32,5 +41,32 @@ export default {
     main{
         background-color: #1C1C1C;
     }
-    
+    .jumbo{
+        background-image: url('/public/jumbotron.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 25dvh;
+    }
+
+    .btnLoad{
+        background-color: #0081F8;
+        border: 0;
+        font-size: 0.5rem;
+        font-weight: 600;
+        &:hover{
+            background-color: #0167c7;
+        }
+    }
+    .labelSection{
+        position: absolute;
+        padding: 7.5px;
+        background-color: #0081F8;
+        border: 0;
+        font-size: 0.75rem;
+        font-weight: 600; 
+        bottom: -5%;
+        left: -4%;
+        transform: translate(50%);
+
+    }
 </style>
--- src/components/partials/AppCard.vue
+++ src/components/partials/AppCard.vue
@@ -10,7 +10,8 @@ export default {
 </script>
 
 <template>
-    <div class="col p-2">
+    <div class="col p-2 comic position-relative">
+        <p class="position-absolute priceComic">{{ price }}</p>
         <img :src="pathImage" :alt="title" class="comicCover">
         <p class="comicTitle mb-3 text-uppercase">{{ title }}</p>
     </div>
@@ -21,4 +22,30 @@ export default {
         font-size: 0.7rem;
         margin-top: 10px;   
     }
+    .comic{
+        cursor: pointer;
+        img{
+            transition: all 0.3s;
+        }
+        .priceComic{
+            bottom: 50%;
+            left: 50%;
+            transform: translateX(-50%);
+            opacity: 0;
+            font-weight: 700;
+            z-index: 100;
+            transition: all 0.5s;
+        }
+        &:hover{
+            img{
+                filter: brightness(0.25);
+                scale: 1.01;
+            }
+            .priceComic{
+                opacity: 1;
+            }
+        }
+    }
+    
+
 </style>
